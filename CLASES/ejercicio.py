e811ca16c920f164e5a5bf51a245b949242f10d0
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-02 17:22:08 +0200

    ahora el correo ya está definido

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -1,8 +1,9 @@
 import re
-s = correo
-re.search(". * @. * \ .. *", s)
+
 print("Introduzca un correo electrónico")
 correo = input()
+s = correo
+re.search(". * @. * \ .. *", s)
 if re.search(". * @. * \ .. *", correo):
     print("El correo es válido")
 else:

ff0d0636bbab488b9d04fa0c612bca2df45bfdba
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-02 17:21:41 +0200

    sefiltra el correo

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -1,3 +1,9 @@
 import re
 s = correo
 re.search(". * @. * \ .. *", s)
+print("Introduzca un correo electrónico")
+correo = input()
+if re.search(". * @. * \ .. *", correo):
+    print("El correo es válido")
+else:
+    print("Podría tratarse de un ciberataque. ")

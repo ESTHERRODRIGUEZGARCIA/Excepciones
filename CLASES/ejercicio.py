bc6e40a152bb4d562cbcb9c78adee3eeb5a3e867
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 09:30:36 +0200

    lo hago con excepciones

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -8,3 +8,21 @@ if re.search(". * @. * \ .. *", correo):
     print("El correo es válido")
 else:
     print("Podría tratarse de un ciberataque. ")
+
+class miExcepcion():
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+    def __str__(self):
+        return self.mensaje
+
+try:
+    print("Introduzca un correo electrónico")
+    correo = input()
+    s = correo
+    re.search(". * @. * \ .. *", s)
+    if re.search(". * @. * \ .. *", correo):
+        print("El correo es válido")
+    else:
+        raise miExcepcion("Podría tratarse de un ciberataque. ")
+except miExcepcion as e:
+    print(e)

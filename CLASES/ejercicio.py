36857d6bedb679e0b662fd3b80376e9b3775950e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 16:03:41 +0200

    no va el fallo

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -1,14 +1,5 @@
 import re
 
-print("Introduzca un correo electrónico")
-correo = input()
-s = correo
-re.search(". * @. * \ .. *", s)
-if re.search(". * @. * \ .. *", correo):
-    print("El correo es válido")
-else:
-    print("Podría tratarse de un ciberataque. ")
-
 class miExcepcion():
     def __init__(self, mensaje):
         self.mensaje = mensaje
@@ -25,4 +16,4 @@ try:
     else:
         raise miExcepcion("Podría tratarse de un ciberataque. ")
 except miExcepcion as e:
-    print(e)
+    print("Excepción capturada: {}".format(e))

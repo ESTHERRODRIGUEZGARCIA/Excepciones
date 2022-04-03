fb614155263cb4ba5237bd483ff3397e57dddf4a
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 16:59:22 +0200

    me da error y noo se qué significa

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -1,6 +1,6 @@
 import re
 
-class miExcepcion(Excepcion):
+class Excepcion():
     def __init__(self, correo): # Este atributo permite tener una información adicional sobre el motivo del desencadenamiento.
         self.correo = correo
     def __str__(self):      #Para poder visualizar una excepción correctamente
@@ -11,17 +11,16 @@ try:
     correo = input()
     s = correo
     re.search(". * @. * \ .. *", s)
-    if re.search(". * @. * \ .. *", correo):
+    if re.search(". * @. * \ .. *", s):
         print("El correo es válido. Bienvenido.")
     else:
-        raise Excepcion(correo, " es una entrada incorrecta. Introduzca una dirección de correo electrónico. ")
+        raise Excepcion(s, " es una entrada incorrecta. Introduzca una dirección de correo electrónico. ")
     try:
-        if re.search(". * @. * \ .. *", correo):
+        if re.search(". * @. * \ .. *", s):
             print("El correo es válido. Bienvenido.")
         else:
             raise Excepcion("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
-    except Excepcion as e:
-        print(e)
+    except Excepcion():
         print("Cuenta bloqueada a causa de un ataque.")
 
 finally:

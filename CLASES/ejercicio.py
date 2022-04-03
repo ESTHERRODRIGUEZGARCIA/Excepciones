5a7aeb505c8eeb3a58866e79428bfdd074686c49
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 17:38:35 +0200

    no se como hacer que detecte good

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -4,14 +4,13 @@ class Excepcion():
     def __init__(self, correo): # Este atributo permite tener una información adicional sobre el motivo del desencadenamiento.
         self.correo = correo
     def __str__(self):
-        return ("El correo es válido. Bienvenido.")
+        pass
 
 try:
     print("Introduzca un correo electrónico: ")
     correo = input()
-    s = correo
-    re.search(". * @. * \ .. *", s)
-    if re.search(". * @. * \ .. *", s):
+    re.search(". * @. * \ .. *", correo)
+    if correo = re.search(". * @. * \ .. *", correo):
         print("El correo es válido. Bienvenido.")
     else:
         raise TypeError("Es una entrada incorrecta. Introduzca una dirección de correo electrónico. Debe tener el formato xxx@xxx.xx ")

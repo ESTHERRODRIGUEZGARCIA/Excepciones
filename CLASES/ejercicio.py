cc9dbb88026483d111b3e6045d65fb669c57cbce
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 16:50:36 +0200

    no es así

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -3,27 +3,25 @@ import re
 class Excepcion():
     def __init__(self, correo):
         self.correo = correo
-    def __str__(self):
-        return self.correo
 
-try:
-    print("Introduzca un correo electrónico: ")
-    correo = input()
-    s = correo
-    re.search(". * @. * \ .. *", s)
-    if re.search(". * @. * \ .. *", correo):
-        print("El correo es válido. Bienvenido.")
-    else:
-        raise Excepcion(correo, " es una entrada incorrecta. Introduzca una dirección de correo electrónico. ")
     try:
+        print("Introduzca un correo electrónico: ")
+        correo = input()
+        s = correo
         re.search(". * @. * \ .. *", s)
         if re.search(". * @. * \ .. *", correo):
             print("El correo es válido. Bienvenido.")
         else:
-            raise Excepcion("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
-    except Excepcion():
-        print("Cuenta bloqueada a causa de un ataque.")
+            raise Excepcion(correo, " es una entrada incorrecta. Introduzca una dirección de correo electrónico. ")
+        try:
+            re.search(". * @. * \ .. *", s)
+            if re.search(". * @. * \ .. *", correo):
+                print("El correo es válido. Bienvenido.")
+            else:
+                raise Excepcion("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
+        except Excepcion():
+            print("Cuenta bloqueada a causa de un ataque.")
 
-finally:
-    print("Gracias por usar nuestro programa.")
+    finally:
+        print("Gracias por usar nuestro programa.")
 

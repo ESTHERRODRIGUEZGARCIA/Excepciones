752a49e93a17aed287041ffa7a8dd7414010f0ca
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-03 16:55:04 +0200

    cambio, 4 problemas maaas

--- CLASES/ejercicio.py
+++ CLASES/ejercicio.py
@@ -1,6 +1,6 @@
 import re
 
-class Excepcion():
+class miExcepcion(Excepcion):
     def __init__(self, correo): # Este atributo permite tener una información adicional sobre el motivo del desencadenamiento.
         self.correo = correo
     def __str__(self):      #Para poder visualizar una excepción correctamente
@@ -20,7 +20,8 @@ try:
             print("El correo es válido. Bienvenido.")
         else:
             raise Excepcion("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
-    except Excepcion():
+    except Excepcion as e:
+        print(e)
         print("Cuenta bloqueada a causa de un ataque.")
 
 finally:
